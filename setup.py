3611f65cafdd480a068d725c3dfff1bde18ed3fa
Luke Nezda <lnezda@gmail.com>
2019-06-27 13:11:00 -0500

    call this 2.0.0

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ with open('LICENSE') as f:
 
 setup(
     name='fold_to_ascii',
-    version='1.0.1',
+    version='2.0.0',
     description='A Python port of the Apache Lucene ASCII Folding Filter that converts alphabetic, numeric, and symbolic Unicode characters which are not in the first 127 ASCII characters (the ‘Basic Latin’ Unicode block) into ASCII equivalents, if they exist.',
     long_description=readme,
     author='William Bert',
     author_email='william@spanishdict.com',
     url='https://github.com/spanishdict/fold_to_ascii',
     license=license,
-    packages=find_packages(exclude=('tests')),
+    packages=find_packages(exclude='tests'),
     keywords="ascii unicode sanitize diacritics fold folding ligatures"
 )
